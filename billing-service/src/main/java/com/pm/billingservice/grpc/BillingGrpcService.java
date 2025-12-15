cd8ff93cb73aeabc628299bbb5301766f86232e2
edadilek <edilek2001@gmail.com>
2025-12-15 23:08:11 +0300

    Initial commit

---
+++ billing-service/src/main/java/com/pm/billingservice/grpc/BillingGrpcService.java
@@ -0,0 +1,14 @@
+package com.pm.billingservice.grpc;
+
+import billing.BillingResponse;
+import billing.BillingServiceGrpc.BillingServiceImplBase;
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+
+@GrpcService
+public class BillingGrpcService extends BillingServiceImplBase {
+    @Override
+    public void createBilingAccount(billing.BillingRequest billingRequest, StreamObserver<BillingResponse> responseObserver){
+
+    }
+}
